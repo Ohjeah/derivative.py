6b55920034fdaa36c1a9992b941891bdf271c384
Markus Quade <markus.quade@gmail.com>
2019-01-23 17:03:41 +0100

    init

---
+++ examples/demo.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from derivative import derivative, methods
+
+
+def f(x):
+    return np.sin(x)
+
+n_points = 150
+noise_amplitude = 0.02
+
+x = np.linspace(0, 6 * np.pi, n_points)
+y = f(x) + noise_amplitude * np.random.normal(size=x.shape)
+
+plt.plot(x, np.cos(x), "x--", label="f'")
+for method in methods:
+    y_deriv = derivative(x, y, kind=method)
+    plt.plot(x, y_deriv, "o--", label=method)
+
+plt.legend()
+plt.show()
